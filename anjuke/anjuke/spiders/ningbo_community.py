9ef7230b45300cf00c29c166b2c0fd42f38013f7
kylin1020 <stRNiMdnWEQEC4E>
2020-01-03 14:11:30 +0800

    [BUG修复]初始url不过滤

--- anjuke/anjuke/spiders/ningbo_community.py
+++ anjuke/anjuke/spiders/ningbo_community.py
@@ -33,7 +33,8 @@ class NingboCommunity(scrapy.Spider):
                     'sec-fetch-user': '?1',
                     'upgrade-insecure-requests': '1',
                     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36',
-                }
+                },
+                dont_filter=True
             )
 
     def parse(self, response):
